d7312e5230db6a6060b8e44b23f0291b92919a06
Antoine Bédex <antoine.bedex@tutanota.com>
2020-01-15 21:10:25 +0100

    début de terminal

--- src/terminal.adb
+++ src/terminal.adb
@@ -1,6 +1,14 @@
-procedure Terminal is
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with P_Constants; use P_Constants;
 
+procedure Terminal is
+   command : String(1..
 begin
+
+   loop
+
+
    --  Insert code here.
    null;
 end Terminal;
