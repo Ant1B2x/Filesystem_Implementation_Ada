c61c2d2cff7dc53b15bdaf48f95d5c4b744d5839
Antoine Bédex <antoine.bedex@tutanota.com>
2020-01-15 16:39:27 +0100

    final parser

--- src/p_parser.adb
+++ src/p_parser.adb
@@ -18,17 +18,17 @@ package body P_Parser is
       substrings := P_Substrings.create;
       i := 1;
       substring_first := 1;
-      while(i <= original'Length) loop
-         if(original(i) = separator)then
+      while i <= original'Length loop
+         if original(i) = separator then
             add_substring(substrings, To_Unbounded_String(original(substring_first..(i - 1))));
-            while(i < original'Length and then original(i + 1) = separator)loop
+            while i < original'Length and then original(i + 1) = separator loop
                i := i + 1;
             end loop;
             substring_first := i + 1;
          end if;
          i := i + 1;
       end loop;
-      if(original(original'Last) /= separator)then
+      if original(original'Last) /= separator then
          add_substring(substrings, To_Unbounded_String(original(substring_first..original'Last)));
       end if;
       return substrings;
@@ -46,48 +46,45 @@ package body P_Parser is
 
    
    procedure run_command(current_dir: T_Folder; command: String) is
- 
       substrings : T_SubStrings;
-      OptionTrue : Boolean := False;
-      firstParameter : Unbounded_String;
-      secondParameter : Unbounded_String;
-      numberOfElement : Integer;
-      
- 
+      option_true : Boolean := False;
+      first_parameter : Unbounded_String;
+      second_parameter : Unbounded_String;
+      num_element : Integer;
    begin
       substrings := split_string(command, ' ');
 
-      numberOfElement := P_Substrings.get_nb_values(substrings);
-      if(numberOfElement > 1)then
-         if(get_substring_to_string(substrings, 2) = "-r")then
-            OptionTrue := True;
-            if(numberOfElement > 2)then
-               firstParameter := substrings.subStrings(3);
-               if(numberOfElement = 4)then
-                  secondParameter := substrings.subStrings(4);
+      num_element := P_Substrings.get_nb_values(substrings);
+      if num_element > 1 then
+         if get_substring_to_string(substrings, 2) = "-r" then
+            option_true := True;
+            if num_element > 2 then
+               first_parameter := get_substring(substrings, 3);
+               if num_element = 4 then
+                  second_parameter := get_substring(substrings, 4);
                end if;
             end if;
          else
-            firstParameter := substrings.subStrings(2);
-            if(numberOfElement = 3)then
-               secondParameter := substrings.subStrings(3);
+            first_parameter := get_substring(substrings, 2);
+            if num_element = 3 then
+               second_parameter := get_substring(substrings, 3);
             end if;
          end if;
       else
-         firstParameter := To_Unbounded_String("");
-         secondParameter := To_Unbounded_String("");
+         first_parameter := To_Unbounded_String("");
+         second_parameter := To_Unbounded_String("");
       end if;
-      case encodedCommands'Value(get_substring_to_string(substrings, 1)) is
-         when ls => lsCommand(OptionTrue, To_String(firstParameter), current_dir);
-         when rm => rmCommand(OptionTrue,To_String(firstParameter), current_dir);
+      case encoded_commands'Value(get_substring_to_string(substrings, 1)) is
+         when ls => lsCommand(option_true, To_String(first_parameter), current_dir);
+         when rm => rmCommand(option_true,To_String(first_parameter), current_dir);
          when pwd => pwdCommand(current_dir);
-         when  cd => cdCommand(To_String(firstParameter), current_dir);
-         when mkdir => mkdirCommand(To_String(firstParameter), current_dir);
-         when  cp => cpCommand(OptionTrue, To_String(firstParameter), current_dir);
-         when  mv => mvCommand(To_String(firstParameter), To_String(secondParameter), current_dir);
-         when  tar => tarCommand(To_String(firstParameter), current_dir);
-         when  touch => touchCommand(To_String(firstParameter), current_dir);
-         when others => Put_Line("> Commande non reconnue.");
+         when  cd => cdCommand(To_String(first_parameter), current_dir);
+         when mkdir => mkdirCommand(To_String(first_parameter), current_dir);
+         when  cp => cpCommand(option_true, To_String(first_parameter), current_dir);
+         when  mv => mvCommand(To_String(first_parameter), To_String(second_parameter), current_dir);
+         when  tar => tarCommand(To_String(first_parameter), current_dir);
+         when  touch => touchCommand(To_String(first_parameter), current_dir);
+         when others => Put_Line("> Unknown command.");
       end case;
    end run_command;
    
