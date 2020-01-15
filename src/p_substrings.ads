e7187f2071eecc067a2ba7c3cce61af2429662d4
Ant1B2x <antoine.bedex@tutanota.com>
2020-01-15 21:20:53 +0100

    subtype and terminal separation

--- src/p_substrings.ads
+++ src/p_substrings.ads
@@ -2,27 +2,21 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with P_Array;
-with P_Folder; use P_Folder;
 
 -- Unbounded_string https://openclassrooms.com/fr/courses/900279-apprenez-a-programmer-avec-ada/894191-les-chaines-de-caracteres
-package P_Parser is
-   
-   package P_Substrings is new P_Array (T => Unbounded_String);   
-   subtype T_Substrings is P_Substrings.T_Array;
-   
+package P_Substrings is
+
+   package P_Substrings_Array is new P_Array (T => Unbounded_String);
+   subtype T_Substrings is P_Substrings_Array.T_Array;
+
    function create_substrings return T_Substrings;
-   
+
    procedure add_substring (substrings : in out T_Substrings; substring : in Unbounded_String);
-   
+
    function split_string (original : in String; separator : in Character) return T_Substrings;
-   
+
    function get_substring (substrings : in T_Substrings; index : in Integer) return Unbounded_String;
-   
+
    function get_substring_to_string (substrings : in T_Substrings; index : in Integer) return String;
-   
-   procedure run_command(current_dir: T_Folder; command: String);
-   
-private
-   type encoded_commands is (ls, rm, pwd, cd, mkdir, cp, mv, tar, touch);
-   
-end P_Parser;
+
+end P_Substrings;
--- src/terminal.adb
+++ src/terminal.adb
@@ -1,14 +1,57 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with P_Constants; use P_Constants;
+with P_Substrings; use P_Substrings;
 
 procedure Terminal is
-   command : String(1..
+
+   type encoded_commands is (ls, rm, pwd, cd, mkdir, cp, mv, tar, touch);
+   procedure run_command(current_dir: T_Folder; command: String) is
+      substrings : T_SubStrings;
+      option_true : Boolean := False;
+      first_parameter : Unbounded_String;
+      second_parameter : Unbounded_String;
+      num_element : Integer;
+   begin
+      substrings := split_string(command, ' ');
+
+      num_element := P_Substrings.get_nb_values(substrings);
+      if num_element > 1 then
+         if get_substring_to_string(substrings, 2) = "-r" then
+            option_true := True;
+            if num_element > 2 then
+               first_parameter := get_substring(substrings, 3);
+               if num_element = 4 then
+                  second_parameter := get_substring(substrings, 4);
+               end if;
+            end if;
+         else
+            first_parameter := get_substring(substrings, 2);
+            if num_element = 3 then
+               second_parameter := get_substring(substrings, 3);
+            end if;
+         end if;
+      else
+         first_parameter := To_Unbounded_String("");
+         second_parameter := To_Unbounded_String("");
+      end if;
+      case encoded_commands'Value(get_substring_to_string(substrings, 1)) is
+         when ls => lsCommand(option_true, To_String(first_parameter), current_dir);
+         when rm => rmCommand(option_true,To_String(first_parameter), current_dir);
+         when pwd => pwdCommand(current_dir);
+         when  cd => cdCommand(To_String(first_parameter), current_dir);
+         when mkdir => mkdirCommand(To_String(first_parameter), current_dir);
+         when  cp => cpCommand(option_true, To_String(first_parameter), current_dir);
+         when  mv => mvCommand(To_String(first_parameter), To_String(second_parameter), current_dir);
+         when  tar => tarCommand(To_String(first_parameter), current_dir);
+         when  touch => touchCommand(To_String(first_parameter), current_dir);
+         when others => Put_Line("> Unknown command.");
+      end case;
+   end run_command;
+
+   command : String(1..LMAX_STRING);
 begin
 
-   loop
-
-
-   --  Insert code here.
+   -- code
    null;
 end Terminal;
