2823de9ec1fd567e10c7daa1a50643b7849bed0f
Ant1B2x <antoine.bedex@tutanota.com>
2020-01-17 15:39:08 +0100

    test substrings

--- src/test_substrings.adb
+++ src/test_substrings.adb
@@ -1,10 +1,10 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with P_Substrings; use P_Substrings;
 
 procedure test_substrings is
    substrings : T_Substrings;
-   char : Character;
 begin
    -- create substrings
    put_line("Create an empty array of substrings:");
@@ -18,22 +18,40 @@ begin
    
    -- split string
    put_line("Split string:");
-   put_line("Splitting ""ab cd ef gh"" as substrings");
-   substrings := split_string("ab/cd/de", '/');
+   put_line("Splitting ""ab/cd/ef/gh/"" as substrings");
+   substrings := split_string("ab/cd/ef/gh/", '/');
+   new_line;
    
    -- get nb substrings
    put_line("Get nb substrings:");
-   put(get_nb_substrings(substrings));
+   put_line("get_nb_substrings(substrings) = " & Integer'Image(get_nb_substrings(substrings)));
+   new_line;
    
+   -- get substring
+   put_line("Get substring (Unbounded_String):");
+   if get_substring(substrings, 2) = To_Unbounded_String("cd") then
+      put_line("get_substring(substrings, 2) = To_Unbounded_String(""cd"")");
+   else
+      put_line("get_substring(substrings, 2) is incoherent");
+   end if;
+   new_line;
    
+   -- get substring to string
+   put_line("Get substring (String):");
+   if get_substring_to_string(substrings, 2) = "cd" then
+      put_line("get_substring_to_string(substrings, 2) = ""cd""");
+   else
+      put_line("get_substring_to_string(substrings, 2) is incoherent");
+   end if;
+   new_line;
 
-   --function split_string (original : in String; separator : in String) return T_Substrings;
-
-   --function get_nb_substrings (substrings : in T_Substrings) return Integer;
-
-   --function get_substring (substrings : in T_Substrings; index : in Integer) return Unbounded_String;
-
-   --function get_substring_to_string (substrings : in T_Substrings; index : in Integer) return String;
-
-   --function get_substrings(substrings: in T_Substrings; index_first: Integer; index_last: Integer) return T_Substrings;
+   -- get substrings
+   put_line("Get substrings:");
+   if get_substring_to_string(get_substrings(substrings, 2, 4), 2) = "ef" then
+      put_line("get_substring_to_string(get_substrings(substrings, 2, 4), 2) = ""ef""");
+   else
+      put_line("get_substrings(substrings, 2, 4) is incoherent");
+   end if;
+   new_line;
+   
 end test_substrings;
